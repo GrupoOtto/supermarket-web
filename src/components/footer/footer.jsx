10346bfc29a9cd8c82dcc825f757dabf8d5f5b52
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-18 16:39:01 -0300

    Implement footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css';
+
+export default props => (
+  <div className="footer">
+    <span>DSSD - Grupo 15 | K supermarket Inc. © 2018</span>
+  </div>
+);
