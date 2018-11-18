2b3903ce862bfb6012f19cea1d7a6affe3326e80
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-18 16:40:04 -0300

    Improve general styling

--- src/components/searcher/searcher.jsx
+++ src/components/searcher/searcher.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Input, Icon } from 'antd';
 
-import './style.css'
+import './style.css';
 
-const searcher = props =>  <Input
+export default props => (
+  <Input
     prefix={<Icon type="search" style={{ color: 'rgba(0,0,0,.25)' }} />}
     className="searcher"
     placeholder="Buscar Productos, Marcas y más..."
-    />
-
-export default searcher;
+  />
+);
