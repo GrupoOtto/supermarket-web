8e3194d70ab420792b72edfca1bc443b457dde42
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-19 15:26:31 -0300

    Add Home page component

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import Product from '../../components/product/product';
+import './style.css';
+
+import products from './products';
+
+class Home extends Component {
+  state = {
+    products: []
+  };
+
+  render() {
+    return (
+      <div className="products-container">
+        {products.map((p, i) => (
+          <Product key={i} {...p} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/pages/routes.jsx
+++ src/pages/routes.jsx
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Header from '../components/header/header';
+import Home from './Home/home';
 import Error from './Error/404';
 
 export default () => (
   <Fragment>
     <Header />
     <Switch>
-      <Route path="/" exact render={() => <div>gdfgfd</div>} />
+      <Route path="/" exact component={Home} />
       <Route component={Error} />
     </Switch>
   </Fragment>
