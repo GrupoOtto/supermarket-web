476810b3bd5f8d637e0cd6347a3a252addfe1a50
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-22 20:32:50 -0300

    Improve Filters section

--- src/pages/Home/components/filters.jsx
+++ src/pages/Home/components/filters.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Card, Slider, Divider } from 'antd';
+import Search from '../../../components/searcher/searcher';
+import './style.css';
 
-export default props => (
-  <Card style={{ marginTop: 10 }}>
-    <Divider />
-    <Slider range defaultValue={[20, 50]} />
-  </Card>
-);
+const { Meta } = Card;
+
+export default props => {
+  const [values, setValues] = useState([0, 100]);
+
+  return (
+    <Card id="filters">
+      <small className="price-selector">Precio</small>
+      <Slider range defaultValues={values} onChange={setValues} />
+      <div>
+        <small>min: ${values[0]}</small>
+        <small>max: ${values[1]}</small>
+      </div>
+    </Card>
+  );
+};
