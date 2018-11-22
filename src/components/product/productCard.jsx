546751123b0f427adebcd8605ba2f1f8c76fabe4
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-22 20:32:27 -0300

    Implement new product Card

--- src/components/product/productCard.jsx
+++ src/components/product/productCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Card, Button } from 'antd';
+import { Card, Button, Icon } from 'antd';
 
 import Price from './components/price';
 import Input from '../input/numberSelector';
@@ -15,7 +15,7 @@ export default props => {
       className="product-card"
       hoverable
       cover={<img alt="product" src={props.image} />}
-      actions={[<div>Más</div>, <div>Agregar al Carrito</div>]}
+      actions={[<Icon type="eye" />, <Icon type="shopping-cart" />]}
     >
       <span className="product-name">{props.name}</span>
       <Price price={props.salePrice} oldPrice={props.oldPrice} />
