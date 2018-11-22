b80608cc41c76e57bfe3822b7ce13df9eb2fb971
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-22 20:33:03 -0300

    Add pagination component to home page

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 import Filters from './components/filters';
 import Product from '../../components/product/productCard';
+import Pagination from '../../components/pagination/pagination';
 
 import './style.css';
 import products from './products';
@@ -14,16 +15,21 @@ class Home extends Component {
 
   render() {
     return (
-      <Row>
-        <Col md={4}>
-          <Filters />
-        </Col>
-        <Col md={17} className="products-container">
-          {products.map((p, i) => (
-            <Product key={i} {...p} />
-          ))}
-        </Col>
-      </Row>
+      <div>
+        <Row style={{ margin: '0px 10px' }}>
+          <Col xl={4} lg={6}>
+            <Filters />
+          </Col>
+          <Col xl={16} lg={18}>
+            <div className="products-container">
+              {products.map((p, i) => (
+                <Product key={i} {...p} />
+              ))}
+            </div>
+            <Pagination defaultCurrent={6} total={500} />
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
