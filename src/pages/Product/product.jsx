62dea73fef4fba5355ac05b98d524bcbca8ffc96
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-25 15:13:03 -0300

    Fix merge

--- src/pages/Product/product.jsx
+++ src/pages/Product/product.jsx
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { showProduct } from '../../store/actions/productsActions';
 
 import { Row } from 'antd';
 import Col from '../../components/grid/col';
 import Carousel from '../../components/carousel/carousel';
 import Details from './components/card/card';
 import RelatedProducts from './components/relatedProducts/relatedProducts';
+import Error from '../Error/404';
 
-import products from '../Home/products';
+const product = props => {
+  const product = props.products.find(
+    x => x._id.toString() === props.match.params.id
+  );
 
-export default class Product extends Component {
-  render() {
-    return (
-      <div style={{ margin: '40px' }}>
-        <Row gutter={48}>
-          <Col md={15}>
-            <Carousel
-              images={[
-                'https://i5.walmartimages.com/asr/cbcc26fb-af25-4672-bfb8-cb156cf1dadf_2.b93305e90bf7f66b9ab5f36f25652660.jpeg?odnWidth=215&odnHeight=215&odnBg=ffffff',
-                'https://i5.walmartimages.com/asr/cbcc26fb-af25-4672-bfb8-cb156cf1dadf_2.b93305e90bf7f66b9ab5f36f25652660.jpeg?odnWidth=215&odnHeight=215&odnBg=ffffff'
-              ]}
-            />
-          </Col>
-          <Col md={9}>
-            <Details {...products[0]} />
-          </Col>
-        </Row>
-        <RelatedProducts products={products.slice(0, 4)} />
-      </div>
-    );
-  }
-}
+  return product ? (
+    <div style={{ margin: '40px' }}>
+      <Row gutter={48}>
+        <Col md={15}>
+          <Carousel images={product.images} />
+        </Col>
+        <Col md={9}>
+          <Details {...product} />
+        </Col>
+      </Row>
+      <RelatedProducts
+        products={props.products.filter(x => x !== product).slice(0, 4)}
+      />
+    </div>
+  ) : (
+    <Error />
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    products: state.productsReducer.products
+  };
+};
+
+export default connect(mapStateToProps)(product);
