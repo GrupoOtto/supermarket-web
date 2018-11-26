28e20b2b1ca43808ffce93f773a1cd0e08650965
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-25 21:36:02 -0300

    Change badge styling

--- src/components/header/navbar/rightSection/menu.jsx
+++ src/components/header/navbar/rightSection/menu.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { Icon, Badge } from 'antd';
 
+import { getCartAmount } from '../../../../store/getters';
+
 import './style.css';
 
-export default () => (
+const menu = props => (
   <div className="navbar-menu">
     <NavLink to="/login">
       <Icon type="user" />
     </NavLink>
     <NavLink to="/cart">
-      <Badge count={5}>
+      <Badge count={props.cartAmount}>
         <Icon type="shopping-cart" />
       </Badge>
     </NavLink>
   </div>
 );
+
+export default connect(getCartAmount)(menu);
