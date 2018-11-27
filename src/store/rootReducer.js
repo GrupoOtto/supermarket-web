3397ba00ca8c1b38197d6d446c894c059eeeb4d3
Matias Pierobon <matias.pierobonm@gmail.com>
2018-11-27 15:45:27 -0300

    import login to redux

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -4,10 +4,12 @@ import productsReducer from './products/reducer';
 import cartReducer from './cart/reducer';
 import uiReducer from './ui/reducer';
 import purchaseReducer from './purchase/reducer';
+import login from './login/reducer';
 
 export default combineReducers({
   productsReducer,
   cartReducer,
   uiReducer,
-  purchaseReducer
+  purchaseReducer,
+  login
 });
