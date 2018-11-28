c05542b28a98b1bb69cda2e74064c9f1314974b3
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-28 17:13:27 -0300

    Remove uiReducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 
 import productsReducer from './products/reducer';
 import cartReducer from './cart/reducer';
-import uiReducer from './ui/reducer';
 import purchaseReducer from './purchase/reducer';
 import login from './login/reducer';
 
 export default combineReducers({
   productsReducer,
   cartReducer,
-  uiReducer,
   purchaseReducer,
   login
 });
