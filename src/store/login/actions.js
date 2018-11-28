99d43ab6feec31db5449a490d2cb5851f85e52fd
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-28 12:41:57 -0300

    Keep user session

--- src/store/login/actions.js
+++ src/store/login/actions.js
@@ -3,9 +3,15 @@ import * as api from '../../lib/api';
 import * as mutations from './mutations';
 
 export const setToken = createAction(mutations.SET_TOKEN);
-export const logout = createAction(mutations.LOGOUT);
+export const logoutUser = createAction(mutations.LOGOUT);
+
+export const logout = () => async (dispatch, getState) => {
+  localStorage.removeItem('token');
+  dispatch(logoutUser());
+};
 
 export const login = (email, password) => async (dispatch, getState) => {
   const response = await api.login(email, password);
+  localStorage.setItem('token', response.data.token);
   dispatch(setToken(response.data.token));
 };
--- src/store/login/reducer.js
+++ src/store/login/reducer.js
@@ -6,6 +6,6 @@ const handlers = {
   [mutations.LOGOUT]: state => ({ ...state, token: null })
 };
 
-const defaultState = { token: null };
+const defaultState = { token: localStorage.getItem('token') };
 
 export default reducer(handlers, defaultState);
