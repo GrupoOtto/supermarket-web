fd1649c270ab2e0bd9aa716773e4cc81e1b9280a
Franco Borrelli <francoborrelli96@outlook.com>
2018-11-29 13:55:12 -0300

    Fetching products values when logging in or out

--- src/store/login/actions.js
+++ src/store/login/actions.js
@@ -10,12 +10,12 @@ export const logoutUser = createAction(mutations.LOGOUT);
 export const logout = () => async (dispatch, getState) => {
   localStorage.removeItem('token');
   dispatch(logoutUser());
-  dispatch(fetchProducts);
+  dispatch(fetchProducts());
 };
 
 export const login = (email, password) => async (dispatch, getState) => {
   const response = await api.login(email, password);
   localStorage.setItem('token', response.data.token);
   dispatch(setToken(response.data.token));
-  dispatch(fetchProducts);
+  dispatch(fetchProducts());
 };
